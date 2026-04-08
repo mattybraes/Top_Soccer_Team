e6e5892539d067665a9f02f866f8b7a36f3bda9f
mattybraes <matthewmeiklebraes@gmail.com>
2026-04-08 09:31:08 +0200

    added teams

--- topsoccerteams/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ topsoccerteams/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,10 +7,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // team list
+    val teams = arrayOf<String>("Mamelodi Sundowns FC",
+        "Bidvest Wits",
+        "Orlando Pirates",
+        "Kaizer Chiefs",
+        "Cape Town City FC")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
